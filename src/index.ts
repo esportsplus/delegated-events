3b34046dfc92832c661014f5c325dbf394443adb
Israel Casillas Jr <ICJR@users.noreply.github.com>
2022-11-27 12:25:37 -0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import { Listener } from './types';
+
+
+let cache: Record<string, WeakMap<Node, Listener>> = {},
+    passive: Record<string, boolean> = {
+        mousedown: true,
+        mouseenter: true,
+        mouseleave: true,
+        mousemove: true,
+        mouseout: true,
+        mouseover: true,
+        mouseup: true,
+        mousewheel: true,
+        scroll: true,
+        touchcancel: true,
+        touchend: true,
+        touchenter: true,
+        touchleave: true,
+        touchmove: true,
+        touchstart: true,
+        wheel: true
+    };
+
+
+const register = (element: Node, event: string, listener: Listener): void => {
+    if (!cache[event]) {
+        cache[event] = new WeakMap();
+
+        document.addEventListener(event, (e) => {
+            let listener,
+                node: Node | null = e.target as Node;
+
+            while (node) {
+                // Element node type
+                if (node.nodeType !== 1) {
+                    break;
+                }
+
+                // Delegated event listener found
+                if (listener = cache[event].get(node)) {
+                    if (!passive[event]) {
+                        e.preventDefault();
+                    }
+
+                    e.stopPropagation();
+                    listener(e, node);
+                    break;
+                }
+
+                // Manual event bubbling if event should bubble
+                if (!e.bubbles) {
+                    break;
+                }
+
+                node = node.parentNode;
+            }
+        }, { capture: true, passive: passive[event] });
+    }
+
+    cache[event].set(element, listener);
+};
+
+
+export default { register };
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+type Listener = (e: Event, ref: Node) => void;
+
+
+export { Listener };
